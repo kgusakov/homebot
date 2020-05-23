b04f315a1f1648ae775400f049cf551079fb25a3
Kirill Gusakov <kgusakov@gmail.com>
2020-05-24 01:04:50 +0300

    Updates storing

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,64 @@
 mod telegram_api;
 mod torrent;
 
-use telegram_api::*;
 use std::env;
-
-use lazy_static::lazy_static;
+use std::fs::OpenOptions;
+use telegram_api::*;
+use std::io::Read;
+use std::io::Write;
 
 pub type ProcessingResult = Result<(), Box<dyn std::error::Error>>;
 
-lazy_static! {
-    static ref TELEGRAM_CLIENT: TelegramClient = {
-        let token = env::var("TELEGRAM_TOKEN")
-            .expect("Provide TELEGRAM_TOKEN environment variable please");
-        telegram_api::TelegramClient::new(token)
-    };
-
-    static ref FILTERS: [fn(&Message) -> ProcessingResult; 1] = [
-        |m| torrent::TorrentFilter::new(&TELEGRAM_CLIENT).process(m)
-    ];
+trait Filter {
+    fn process(&self, m: &Message) -> ProcessingResult;
 }
 
 fn main() -> ProcessingResult {
-    let resp = TELEGRAM_CLIENT.get_updates()?;
-    process_updates(resp.result, &FILTERS)
+    let telegram_client: TelegramClient = {
+        let token =
+            env::var("TELEGRAM_TOKEN").expect("Provide TELEGRAM_TOKEN environment variable please");
+        telegram_api::TelegramClient::new(token)
+    };
+
+    let state_file_path =
+        env::var("BOT_STATE_PATH").expect("Provide BOT_STATE_PATH environment variable please");
+    
+
+    let mut update_id = {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .open(&state_file_path)
+            .expect("Can't open file with bot state");
+
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+        contents.trim().parse::<i32>().expect("Bot state is corrupted")
+    };
+
+    // let filters = filters(&telegram_client);
+    let filters = vec![Box::new(torrent::TorrentFilter::new(&telegram_client))];
+
+    loop {
+        let resp = telegram_client.get_updates(update_id + 1)?;
+        process_updates(&resp.result, &filters)?;
+        update_id = resp.result.iter().map(|u| u.update_id).max().unwrap_or(update_id);
+        save_update_id(&state_file_path, update_id);
+    }
 }
 
-fn process_updates(updates: Vec<Update>, handlers: &FILTERS) -> ProcessingResult {
+fn process_updates<'a, T: ?Sized>(updates: &Vec<Update>, handlers: &Vec<Box<T>>) -> ProcessingResult where T: Filter {
     for update in updates.iter() {
         for handler in handlers.iter() {
-            handler(&update.message)?;
+            handler.process(&update.message)?;
         }
     }
     Ok(())
+}
+
+fn save_update_id(state_path: &str, update_id: i32) {
+    let mut f = OpenOptions::new()
+            .write(true)
+            .open(state_path)
+            .expect("Can't open file with bot state for writing new state");
+    f.write_all(update_id.to_string().as_bytes()).expect("Can't save new state to bot state file");
 }
--- src/telegram_api.rs
+++ src/telegram_api.rs
@@ -1,158 +1,165 @@
-use serde::Deserialize;
-use reqwest::blocking::{get};
-use std::fmt;
 use bytes::Bytes;
+use reqwest::blocking::get;
+use serde::Deserialize;
+use std::fmt;
 
 #[derive(Debug, Deserialize)]
 pub struct TelegramResponse<T> {
-  pub ok: bool,
-  pub result: T
+    pub ok: bool,
+    pub result: T,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Update {
-  pub update_id: i32,
-  pub message: Message
+    pub update_id: i32,
+    pub message: Message,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Message {
-  #[serde(default)]
-  pub from: Option<User>,
-  #[serde(default)]
-  pub text: Option<String>,
-  #[serde(default)]
-  pub document: Option<Document>,
-  pub chat: Chat
+    #[serde(default)]
+    pub from: Option<User>,
+    #[serde(default)]
+    pub text: Option<String>,
+    #[serde(default)]
+    pub document: Option<Document>,
+    pub chat: Chat,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Chat {
-  pub id: i64,
+    pub id: i64,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Document {
-  pub file_id: String,
-  pub file_name: String,
-  pub mime_type: String
+    pub file_id: String,
+    pub file_name: String,
+    pub mime_type: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct User {
-  pub id: i32,
-  pub is_bot: bool,
-  pub first_name: String,
-  #[serde(default)]
-  pub last_name: Option<String>,
-  #[serde(default)]
-  pub username: Option<String>
+    pub id: i32,
+    pub is_bot: bool,
+    pub first_name: String,
+    #[serde(default)]
+    pub last_name: Option<String>,
+    #[serde(default)]
+    pub username: Option<String>,
 }
 #[derive(Debug, Deserialize)]
 pub struct File {
-  pub file_id: String,
-  pub file_unique_id: String,
-  pub file_size: Option<i32>,
-  pub file_path: String
+    pub file_id: String,
+    pub file_unique_id: String,
+    pub file_size: Option<i32>,
+    pub file_path: String,
 }
 
-
-
 pub struct TelegramClient {
-  token: String,
-  http_client: reqwest::blocking::Client
+    token: String,
+    http_client: reqwest::blocking::Client,
 }
 
 #[derive(Debug)]
 pub enum FileDownloadError {
-  IoError(std::io::Error),
-  HttpError(reqwest::Error)
+    IoError(std::io::Error),
+    HttpError(reqwest::Error),
 }
 
 impl From<std::io::Error> for FileDownloadError {
-  fn from(err: std::io::Error) -> Self {
-    FileDownloadError::IoError(err)
-  }
+    fn from(err: std::io::Error) -> Self {
+        FileDownloadError::IoError(err)
+    }
 }
 
 impl From<reqwest::Error> for FileDownloadError {
-  fn from(err: reqwest::Error) -> Self {
-    FileDownloadError::HttpError(err)
-  }
+    fn from(err: reqwest::Error) -> Self {
+        FileDownloadError::HttpError(err)
+    }
 }
 
 impl fmt::Display for FileDownloadError {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    let cause_error_msg = match self {
-      FileDownloadError::IoError(e) => format!("{}", e),
-      FileDownloadError::HttpError(e) => format!("{}", e)
-    };
-    write!(f, "Error while trying to process telegram client call {}", cause_error_msg)
-  }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let cause_error_msg = match self {
+            FileDownloadError::IoError(e) => format!("{}", e),
+            FileDownloadError::HttpError(e) => format!("{}", e),
+        };
+        write!(
+            f,
+            "Error while trying to process telegram client call {}",
+            cause_error_msg
+        )
+    }
 }
 
 // This is important for other errors to wrap this one.
 impl std::error::Error for FileDownloadError {
-  fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-      // Generic error, underlying cause isn't tracked.
-      match self {
-        FileDownloadError::IoError(e) => Some(e),
-        FileDownloadError::HttpError(e)  => Some(e),
-      }
-  }
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        // Generic error, underlying cause isn't tracked.
+        match self {
+            FileDownloadError::IoError(e) => Some(e),
+            FileDownloadError::HttpError(e) => Some(e),
+        }
+    }
 }
 
 impl TelegramClient {
+    const BASE_TELEGRAM_API_URL: &'static str = "https://api.telegram.org/bot";
+    const BASE_FILE_TELEGRAM_API_URL: &'static str = "https://api.telegram.org/file/bot";
 
-  const BASE_TELEGRAM_API_URL: &'static str = "https://api.telegram.org/bot";
-  const BASE_FILE_TELEGRAM_API_URL: &'static str = "https://api.telegram.org/file/bot";
-
-  fn api_url(&self, method: &str) -> String {
-      format!("{}{}/{}",
-        TelegramClient::BASE_TELEGRAM_API_URL,
-        self.token,
-        method)
-  }
-
-  fn file_api_url(&self, path: &str) -> String {
-    format!("{}{}/{}",
-      TelegramClient::BASE_FILE_TELEGRAM_API_URL,
-      self.token,
-      path)
-}
-
-  pub fn new(token_value: String) -> TelegramClient {
-    TelegramClient {
-      token: token_value,
-      http_client: reqwest::blocking::Client::new()
+    fn api_url(&self, method: &str) -> String {
+        format!(
+            "{}{}/{}",
+            TelegramClient::BASE_TELEGRAM_API_URL,
+            self.token,
+            method
+        )
     }
-  }
 
+    fn file_api_url(&self, path: &str) -> String {
+        format!(
+            "{}{}/{}",
+            TelegramClient::BASE_FILE_TELEGRAM_API_URL,
+            self.token,
+            path
+        )
+    }
 
-  pub fn get_updates(&self) ->  reqwest::Result<TelegramResponse<Vec<Update>>> {
-    get(&self.api_url("getUpdates"))?
-          .json()
-  }
+    pub fn new(token_value: String) -> TelegramClient {
+        TelegramClient {
+            token: token_value,
+            http_client: reqwest::blocking::Client::new(),
+        }
+    }
 
-  pub fn get_file(&self, file_id: &str) -> reqwest::Result<TelegramResponse<File>> {
-    get(&self.api_url(&format!("getFile?file_id={}", file_id)))?
-          .json()
-  }
+    pub fn get_updates(&self, update_id: i32) -> reqwest::Result<TelegramResponse<Vec<Update>>> {
+        get(&self.api_url(&format!("getUpdates?offset={:?}", update_id)))?.json()
+    }
 
-  pub fn send_message(&self, chat_id: i64, text: String) -> Result<(), Box<dyn std::error::Error>> {
-    let mut body = std::collections::HashMap::<String, String>::new();
-    body.insert("chat_id".to_string(), chat_id.to_string());
-    body.insert("text".to_string(), text);
-    let json_body = serde_json::to_string(&body);
-    self.http_client
-      .post(&self.api_url("sendMessage"))
-      .body(json_body?)
-      .header(reqwest::header::CONTENT_TYPE, "application/json")
-      .send().map_err(|e| e.into()).map(|_| ())
-  }
+    pub fn get_file(&self, file_id: &str) -> reqwest::Result<TelegramResponse<File>> {
+        get(&self.api_url(&format!("getFile?file_id={}", file_id)))?.json()
+    }
 
-  pub fn donwload_file<'a>(&self, file_path: &str) -> Result<Bytes, FileDownloadError> {
-    Ok(get(&self.file_api_url(file_path))?.bytes()?)
-  }
+    pub fn send_message(
+        &self,
+        chat_id: i64,
+        text: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let mut body = std::collections::HashMap::<String, String>::new();
+        body.insert("chat_id".to_string(), chat_id.to_string());
+        body.insert("text".to_string(), text);
+        let json_body = serde_json::to_string(&body);
+        self.http_client
+            .post(&self.api_url("sendMessage"))
+            .body(json_body?)
+            .header(reqwest::header::CONTENT_TYPE, "application/json")
+            .send()
+            .map_err(|e| e.into())
+            .map(|_| ())
+    }
+
+    pub fn donwload_file<'a>(&self, file_path: &str) -> Result<Bytes, FileDownloadError> {
+        Ok(get(&self.file_api_url(file_path))?.bytes()?)
+    }
 }
-
